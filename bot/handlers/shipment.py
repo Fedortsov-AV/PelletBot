496467d94c575dab26c7e5888334b5bf4f0c2499
ForUs <aleksandrfedorcov431@gmail.com>
2026-04-25 08:46:54 +0300

    Мелкие доделки

--- bot/handlers/shipment.py
+++ bot/handlers/shipment.py
@@ -1,12 +1,11 @@
 from aiogram import Router
 from aiogram import types, F
 from aiogram.fsm.context import FSMContext
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot.fsm.shipment import ShipmentStates
 from bot.keyboards.shipment import shipment_main_keyboard, shipment_product_keyboard, shipment_add_more_keyboard
-from bot.services.shipment import save_shipment, get_available_products, create_shipment, add_product_to_shipment, \
+from bot.services.shipment import get_available_products, create_shipment, add_product_to_shipment, \
     get_shipment_products, get_user_shipments, complete_shipment
 from bot.services.wrapers import restrict_anonymous
 
@@ -40,14 +39,14 @@ async def start_adding_shipment(callback: types.CallbackQuery, state: FSMContext
 
     await callback.message.answer(
         "Выберите товар для добавления в отгрузку:",
-        reply_markup= await  shipment_product_keyboard(session)
+        reply_markup=await  shipment_product_keyboard(session)
     )
     await callback.answer()
 
 
 @router.callback_query(F.data.startswith("product_"), ShipmentStates.waiting_for_product)
 @restrict_anonymous
-async def process_product_selection(callback: types.CallbackQuery, state: FSMContext, session: AsyncSession):
+async def process_product_selection(callback: types.CallbackQuery, state: FSMContext):
     product_id = int(callback.data.split("_")[1])
     await state.update_data(selected_product_id=product_id)
     await state.set_state(ShipmentStates.waiting_for_quantity)
@@ -129,7 +128,6 @@ async def finish_shipment(callback: types.CallbackQuery, state: FSMContext, sess
 @router.callback_query(F.data == "view_shipments")
 @restrict_anonymous
 async def show_shipments(callback: types.CallbackQuery, session: AsyncSession):
-
     shipments = await get_user_shipments(session, callback.from_user.id)
 
     if not shipments:
@@ -145,131 +143,8 @@ async def show_shipments(callback: types.CallbackQuery, session: AsyncSession):
 
         await callback.message.answer(text)
 
-    # await callback.answer()
-
-
-# @router.callback_query(F.data == "add_shipment")
-# @restrict_anonymous
-# async def start_shipment_process(
-#         callback: types.CallbackQuery,
-#         state: FSMContext,
-#         session: AsyncSession
-# ):
-#     """Начало процесса отгрузки"""
-#     await callback.answer()
-#
-#     products = await get_available_products(session)
-#
-#     # Создаем кнопки для каждого продукта
-#     buttons = []
-#     for product, amount in products:
-#         buttons.append(
-#             [InlineKeyboardButton(
-#                 text=f"{product.name}\n"
-#                      f" (остаток: {amount})",
-#                 callback_data=f"select_product:{product.id}"
-#             )]
-#         )
-#
-#     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-#
-#     await callback.message.answer(
-#         "Выберите продукт для отгрузки:",
-#         reply_markup=keyboard
-#     )
-#     await state.set_state(ShipmentState.selecting_product)
-#
-#
-# @router.callback_query(F.data.startswith("select_product:"), ShipmentState.selecting_product)
-# @restrict_anonymous
-# async def select_product_for_shipment(
-#         callback: types.CallbackQuery,
-#         state: FSMContext,
-#         session: AsyncSession
-# ):
-#     """Обработка выбора продукта"""
-#     product_id = int(callback.data.split(":")[1])
-#     await state.update_data(product_id=product_id)
-#     await callback.message.answer("Введите количество для отгрузки:")
-#     await state.set_state(ShipmentState.entering_quantity)
-#     await callback.answer()
-#
-#
-# @router.message(ShipmentState.entering_quantity)
-# @restrict_anonymous
-# async def enter_shipment_quantity(
-#         message: types.Message,
-#         state: FSMContext,
-#         session: AsyncSession
-# ):
-#     """Обработка ввода количества"""
-#     try:
-#         quantity = int(message.text)
-#         if quantity <= 0:
-#             raise ValueError
-#     except ValueError:
-#         await message.answer("Пожалуйста, введите целое положительное число.")
-#         return
-#
-#     data = await state.get_data()
-#     product_id = data['product_id']
-#
-#     try:
-#         await save_shipment(
-#             telegram_id=message.from_user.id,  # Передаем telegram_id вместо user_id
-#             product_id=product_id,
-#             quantity=quantity,
-#             session=session
-#         )
-#
-#         keyboard = InlineKeyboardMarkup(
-#             inline_keyboard=[
-#                 [
-#                     InlineKeyboardButton(text="✅ Да", callback_data="add_more"),
-#                     InlineKeyboardButton(text="❌ Нет", callback_data="finish_shipment")
-#                 ]
-#             ]
-#         )
-#
-#         await message.answer(
-#             "Отгрузка успешно добавлена. Добавить еще продукты?",
-#             reply_markup=keyboard
-#         )
-#         await state.set_state(ShipmentState.adding_more)
-#
-#     except ValueError as e:
-#         await message.answer(str(e))
-#         await state.clear()
-#
-#
-# @router.callback_query(F.data == "add_more", ShipmentState.adding_more)
-# @restrict_anonymous
-# async def add_more_products(
-#         callback: types.CallbackQuery,
-#         state: FSMContext,
-#         session: AsyncSession,
-#         **kwargs
-# ):
-#     # print("Доступные аргументы:", kwargs.keys())
-#     """Добавление дополнительных продуктов в отгрузку"""
-#     await start_shipment_process(callback, state, session=session)
-#
-#
-# @router.callback_query(F.data == "finish_shipment", ShipmentState.adding_more)
-# @restrict_anonymous
-# async def finish_shipment_process(
-#         callback: types.CallbackQuery,
-#         state: FSMContext,
-#         session: AsyncSession
-# ):
-#     """Завершение процесса отгрузки"""
-#     await callback.message.answer("Отгрузка завершена.")
-#     await state.clear()
-#     await callback.answer()
-
 
 @router.callback_query(F.data == "close_menu")
 async def close_menu(callback: types.CallbackQuery):
     """Закрытие меню"""
     await callback.message.delete()
-
